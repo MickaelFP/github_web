69cbc9bd369315b53cb7fdcee52b20be252720d6
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-16 14:37:59 +0100

    Create Pongall.js

---
+++ JS/Pongall.js
@@ -0,0 +1,113 @@
+//creer la classe balle
+class Balle{
+    constructor($html){
+        this.$html=$html;
+        this.haut=parseInt($html.css("top"));
+        this.gauche=parseInt($html.css("left"));
+        this.vitesseX=Math.random() * 3;
+        this.vitesseY=Math.random() * 2;
+        this.diametre=$html.height();
+    }
+    majHTML(){
+        this.$html.css("left",balle.gauche);
+        this.$html.css("top",balle.haut); 
+    }
+}
+
+//creer les raquettes avec une seule classe pour 2
+class Raquette{
+    constructor($html){
+        this.$html=$html;
+        this.haut=parseInt(this.$html.css("top"));
+        this.gauche=parseInt(this.$html.css("left"));
+        this.vitesse=1;
+        this.direction=-1;
+    }
+    majHTML(){
+        this.$html.css("top",this.haut);
+    }
+}
+
+// creer le terrain
+class Terrain{
+    constructor($html){
+        this.$html=$html;
+        this.largeur=$html.width();
+        this.hauteur=$html.height();
+    }
+}
+
+// parametres de la balle
+let largeur=$("#balle").width();
+let gauche=parseInt($("#balle").css("left"));
+let haut=parseInt($("#balle").css("top"));
+let Raqhauteur2=$("#raquette2").height();
+let Raqhauteur1=$("#raquette1").height();
+
+// creer un objet en le reliant a la classe 
+let raquette1=new Raquette($("#raquette1"))
+console.log(raquette1);
+
+let raquette2=new Raquette($("#raquette2"))
+console.log(raquette2);
+raquette2.direction = 1
+
+let terrain=new Terrain($("#terrain"))
+console.log(terrain);
+
+let balle=new Balle($("#balle"))
+console.log(balle);
+
+// faire une boucle (rebond de la balle + mouvement des raquettes)
+let i=0;
+setInterval(function(){
+    balle.gauche=balle.gauche+balle.vitesseX;
+    balle.haut=balle.haut+balle.vitesseY;
+    console.log(terrain.largeur)
+    if(balle.gauche>terrain.largeur){
+        balle.gauche=terrain.largeur;
+        balle.gauche = terrain.largeur-balle.diametre;
+        balle.vitesseX=balle.vitesseX*-1;
+    }
+    if(balle.gauche<0){
+        balle.gauche=0;
+        balle.vitesseX=balle.vitesseX*-1;
+    }
+    if(balle.haut>terrain.hauteur){
+        balle.haut=terrain.hauteur;
+        balle.haut=terrain.hauteur-balle.diametre;
+        balle.vitesseY=balle.vitesseY*-1;
+    }
+    if(balle.haut<0){
+        balle.haut=0;
+        balle.vitesseY=balle.vitesseY*-1;
+    }
+
+
+    raquette1.haut=raquette1.haut+raquette1.vitesse*raquette1.direction;
+    raquette2.haut=raquette2.haut+raquette2.vitesse*raquette2.direction;
+
+
+    if(raquette1.haut + Raqhauteur1 >terrain.hauteur){
+        raquette1.haut=terrain.hauteur - Raqhauteur1;
+        raquette1.direction=raquette1.direction*-1;
+    }
+    if(raquette1.haut<0){
+        raquette1.haut=0;
+        raquette1.direction=raquette1.direction*-1;
+    }
+    if(raquette2.haut + Raqhauteur2 >terrain.hauteur){
+        raquette2.haut=terrain.hauteur - Raqhauteur2;
+        raquette2.direction=raquette2.direction*-1;
+    }
+    if(raquette2.haut<0){
+        raquette2.haut=0;
+        raquette2.direction=raquette2.direction*-1;
+    }
+
+    balle.majHTML();
+    raquette1.majHTML();
+    raquette2.majHTML();
+
+
+}, 10);
